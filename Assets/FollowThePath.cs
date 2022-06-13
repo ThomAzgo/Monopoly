a1b2114a68b7cc1466aaa992ff6ad4805e9264de
ThomAzgo <weifang.buisson@gmail.com>
2022-06-13 11:58:08 +0200

    FollowThePath

--- Assets/FollowThePath.cs
+++ Assets/FollowThePath.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Path : MonoBehaviour
+public class FollowThePath : MonoBehaviour
 {
     public Transform[] waypoints;
 
